3168b9975dfbe1f228a19f3b665669ab602dfca8
ericlarosa <eric@rosa.la>
2019-11-24 09:30:34 -0300

    add option for current branch

--- lib/peekj/cli.rb
+++ lib/peekj/cli.rb
@@ -3,14 +3,6 @@ require 'thor'
 
 module Peekj
   class CLI < Thor
-    desc "summary ITEM", "Prints issue summary"
-    def summary(issue_key)
-      issue = JiraApi.get_issue(issue_key)
-
-      say(issue.summary)
-    end
-
-
     desc "login", "Enter login credentials"
     def login
       Credentials::KEYS.each do |credential_key|
@@ -18,6 +10,22 @@ module Peekj
 
         Credentials.send("#{credential_key}=", credential_value)
       end
+    end
+
+    desc "summary ITEM", "Prints issue summary"
+    method_options current_branch: :boolean
+    def summary(issue_key=nil)
+      issue_key = current_branch_issue_key if options['current_branch']
+
+      issue = JiraApi.get_issue(issue_key)
+      say(issue.summary)
+    end
+
+    private
+
+    def current_branch_issue_key
+      current_branch = `git rev-parse --abbrev-ref HEAD`
+      current_branch.match(/[^-]*-[^-]*/)
     end
   end
 end
